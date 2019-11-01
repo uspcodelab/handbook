6b7a0a94d7bd3991947e7750acb37aed4e079f4e
Ygor Tavela <ygortavela@gmail.com>
2019-11-01 18:06:50 -0300

    add sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,11 @@
 module.exports = {
   title: "USPCodeLab Handbook",
   description:
-    "Bem vindo ao Handbook do USPCodeLab! Aqui você pode saber mais sobre este grupo de extensão inter-campi, o que fazemos e como nos organizamos :)"
+    "Bem vindo ao Handbook do USPCodeLab! Aqui você pode saber mais sobre este grupo de extensão inter-campi, o que fazemos e como nos organizamos :)",
+  themeConfig: {
+    sidebar: [
+      '/',
+    ],
+    smoothScroll: true
+  }
 };
